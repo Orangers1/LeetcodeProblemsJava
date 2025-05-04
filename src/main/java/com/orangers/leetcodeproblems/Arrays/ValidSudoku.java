01498e3be84b625b16cb311bc2d7f5dd70d550c8
Josh (Orangers) Eggett <joshuaoeggett@gmail.com>
2025-05-04 01:09:59 -0600

    update valid sudoku to be a little cleaner

--- src/main/java/com/orangers/leetcodeproblems/Arrays/ValidSudoku.java
+++ src/main/java/com/orangers/leetcodeproblems/Arrays/ValidSudoku.java
@@ -6,9 +6,8 @@ import java.util.HashSet;
  * problem can be found at: <a href="https://neetcode.io/problems/valid-sudoku"> problem link </a>
  */
 public class ValidSudoku {
-
     /**
-     * problem can be found at: <a href="https://neetcode.io/problems/valid-sudoku"> neetcode </a> <br>
+     * * problem can be found at: <a href="https://neetcode.io/problems/valid-sudoku"> neetcode </a> <br>
      * Checks if a given sudoku board is valid - is valid if follows the following rules:
      *  <li>Each row must contain the digits 1-9 without duplicates. </li>
      *  <li> Each column must contain the digits 1-9 without duplicates. </li>
@@ -16,23 +15,24 @@ public class ValidSudoku {
      *  <li> Note: a board does not need to be full or solvable to be valid. </li>
      * @param board - the sudoku board (9x9 board)
      * @return whether the board is valid or not
-     * assumes that input is a 9x9 character array as problem states, solution is in o(n^2) time, where n is the number of rows in the input
      */
     public boolean isValidSudoku(char[][] board) {
+        // use set to track digits already seen
         HashSet<Character> seenDigits = new HashSet<>();
         // check for if each row contains digits 1-9 without duplicates
-        for (int i = 0; i < board.length; i++) {
+        for (char[] chars : board) {
             seenDigits.clear();
-            for(int j = 0; j < board[i].length; j++) {
-                if (seenDigits.contains(board[i][j])) {
+            for (char aChar : chars) {
+                if (seenDigits.contains(aChar)) {
                     return false;
-                } else{
-                    if(board[i][j] != '.') { // ignore empty spaces for this check
-                        seenDigits.add(board[i][j]);
+                } else {
+                    if (aChar != '.') { // ignore empty spaces for this check
+                        seenDigits.add(aChar);
                     }
                 }
             }
         }
+
         // same code as above but swap the accesses for board to use j,i instead of i,j to effectively go by columns first
         for (int i = 0; i < board.length; i++) {
             seenDigits.clear();
@@ -46,8 +46,8 @@ public class ValidSudoku {
                 }
             }
         }
-        // if here then columns and rows are valid, now just check the 9 3x3 sub-boxes.
 
+        // if here then columns and rows are valid, now just check the 9 3x3 sub-boxes.
         for (int i = 0 ; i < 3; i++) { // loop through 3 rows of sub-boxes
             for (int j = 0; j < 3; j++) { // loop through 3 columns of sub-boxes
                 seenDigits.clear();
